10bfd379b1b383514a20a03a48a2eb6b3d508aaf
jlz014@ucsd.edu <jlz014@ucsd.edu>
2024-01-02 15:16:09 -0500

    code update

--- code/run_DeepSTARR_ensemble_saliency_analysis_downsampled.sh
+++ code/run_DeepSTARR_ensemble_saliency_analysis_downsampled.sh
@@ -1,6 +1,9 @@
 # run saliency analysis for top 500 Dev enhancers on an ensemble of DeepSTARR models trained on DOWNSAMPLED training data (X_train)
+# set DISTILLED to perform attribution analysis for distilled models
+# set METHOD as saliency or shap to define method of attribution analysis
 
 DISTILLED=true # toggle this flag 
+METHOD=saliency # set as saliency or shap
 DOWNSAMPLE_ARR=( 0.1 0.25 0.5 0.75 )
 N_MODS=10
 PROJ_DIR=../results/DeepSTARR_lr-decay/
@@ -8,12 +11,12 @@ PROJ_DIR=../results/DeepSTARR_lr-decay/
 # # MODELS_DIR=../results/DeepSTARR_lr-decay
 # MODELS_DIR=../results/DeepSTARR_lr-decay/ensemble_distilled
 DATA=../data/DeepSTARR/Sequences_activity_all.h5
-METHOD=saliency
+
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
 # # CUDA_VISIBLE_DEVICES=4,5,6 python DeepSTARR_ensemble_attr_analysis.py --model_dir $MODELS_DIR --n_mods $N_MODS --data $DATA --average
-# CUDA_VISIBLE_DEVICES=4,5,6 python DeepSTARR_ensemble_attr_analysis.py --model_dir $MODELS_DIR --n_mods $N_MODS --data $DATA --method $method
+# CUDA_VISIBLE_DEVICES=4,5,6 python DeepSTARR_ensemble_attr_analysis.py --model_dir $MODELS_DIR --n_mods $N_MODS --data $DATA --method $METHOD
 
 if [ "$DISTILLED" = true ]; then
 	echo "analyzing distilled downsampled models"
