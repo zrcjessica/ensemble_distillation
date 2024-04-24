fcf11d68033c0972ac60e4c4bcdfcb157200158e
jlz014@ucsd.edu <jlz014@ucsd.edu>
2024-04-24 14:51:59 -0400

    restore code and change a path name

--- code/train_DeepSTARR_distilled_downsampled.sh
+++ code/train_DeepSTARR_distilled_downsampled.sh
@@ -1,7 +1,8 @@
 # train an ensemble distilled DeepSTARR model for all downsampled ensembles using distilled training data
 
 N_MODS=10
-DOWNSAMPLE_ARR=( 0.1 0.25 0.5 0.75 )
+# DOWNSAMPLE_ARR=( 0.1 0.25 0.5 0.75 )
+DOWNSAMPLE_ARR=( 0.5 )
 MODELS_DIR=../results/DeepSTARR_lr-decay
 DATA=../data/DeepSTARR/Sequences_activity_all.h5
 CONFIG=../config/DeepSTARR.yaml
