78b416ab8429e8e229b1228b59bb7d8f2891710f
jlz014@ucsd.edu <jlz014@ucsd.edu>
2024-05-29 12:56:06 -0400

    update distillation code for DeepSTARR

--- code/distill_DeepSTARR_with_std.sh
+++ code/distill_DeepSTARR_with_std.sh
@@ -1,33 +1,35 @@
 # trains a single distilled DeepSTARR model that predicts uncertainty (stdev) and mean
 
-# OUTDIR=../results/DeepSTARR_lr-decay/distilled_with_std
-OUTDIR=../results/DeepSTARR_lr-decay
+OUTDIR=../results/DeepSTARR_lr-decay/distilled_with_std
+# OUTDIR=../results/DeepSTARR_lr-decay
 DATA_DIR=../data/DeepSTARR
 DATA=${DATA_DIR}/all_data_with_ensemble_metrics_hierarchical.h5
 CONFIG=../config/DeepSTARR.yaml
 PROJECT_NAME=DeepSTARR_distilled_with_std
-NMODS=1
+NMODS=10
 
 ### boolean vars (toggle true/false)
 # train w/ evoaug
 evoaug=false
 if [ "$evoaug" = true ]; then
-    # OUTDIR=../results/DeepSTARR_evoaug/distilled_with_std
-	OUTDIR=../results/DeepSTARR_evoaug
+    OUTDIR=../results/DeepSTARR_evoaug/distilled_with_std
+	# OUTDIR=../results/DeepSTARR_evoaug
 	DATA=${DATA_DIR}/evoaug/all_data_with_ensemble_metrics_hierarchical.h5
 fi
-downsample=false 
+downsample=true 
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/
 
 if [ "$downsample" = true ]; then
-	# analyze downsampled models
+	# train downsampled models
+	echo "training downsampled models"
 	DOWNSAMPLE_ARR=( 0.1 0.25 0.5 0.75 )
 	for p in "${!DOWNSAMPLE_ARR[@]}"
 	do	
-		DOWNSAMPLE_OUTDIR=${OUTDIR}/downsample_${DOWNSAMPLE_ARR[$p]}/distilled_with_std
-		DOWNSAMPLE_DATA=${DATA_DIR}/downsample_${DOWNSAMPLE_ARR[$p]}_all_data_with_ensemble_metrics_hierarchical.h5
+		DOWNSAMPLE_OUTDIR=${OUTDIR}/downsample_${DOWNSAMPLE_ARR[$p]}
+		DOWNSAMPLE_DATA=${DATA_DIR}/downsample${DOWNSAMPLE_ARR[$p]}_all_data_with_ensemble_metrics_hierarchical.h5
 		mkdir -p $DOWNSAMPLE_OUTDIR
+		echo "downsample p = ${DOWNSAMPLE_ARR[$p]}"
 		for i in $(seq 1 $NMODS)
 		do
 			if [ "$evoaug" = true ]; then
@@ -48,7 +50,7 @@ else
 		else
 			echo "python train_stdev_DeepSTARR.py --ix $i --out $OUTDIR --data $DATA --plot --config $CONFIG --project $PROJECT_NAME --lr_decay"
 		fi
-	done | simple_gpu_scheduler --gpus 0
+	done | simple_gpu_scheduler --gpus 0,1,2,3,4
 fi
 
 # message the user on slack if possible
@@ -56,15 +58,31 @@ exit_code="$?"
 if command -v 'slack' &>/dev/null; then
     if [ "$exit_code" -eq 0 ]; then
 		if [ "$evoaug" = true ]; then
-			slack "training distilled DeepSTARR models (+EvoAug) with standard deviation prediction completed successfully" &>/dev/null
+			if [ "$downsample" = true ]; then 
+				slack "training downsampled distilled DeepSTARR models (+EvoAug) with standard deviation prediction completed successfully" &>/dev/null
+			else 
+				slack "training distilled DeepSTARR models (+EvoAug) with standard deviation prediction completed successfully" &>/dev/null
+			fi
 		else
-			slack "training distilled DeepSTARR models with standard deviation prediction completed successfully" &>/dev/null
+			if [ "$downsample" = true ]; then 
+				slack "training downsampled distilled DeepSTARR models with standard deviation prediction completed successfully" &>/dev/null
+			else 
+				slack "training distilled DeepSTARR models with standard deviation prediction completed successfully" &>/dev/null
+			fi
 		fi
 	else
 		if [ "$evoaug" = true ]; then
-			slack "training distilled DeepSTARR models (+EvoAug) with standard deviation prediction  exited with error code $exit_code"
-		else 
-			slack "training distilled DeepSTARR models with standard deviation prediction  exited with error code $exit_code"
+			if [ "$downsample" = true ]; then 
+				slack "training downsampled distilled DeepSTARR models (+EvoAug) with standard deviation prediction  exited with error code $exit_code"
+			else 
+				slack "training distilled DeepSTARR models (+EvoAug) with standard deviation prediction  exited with error code $exit_code"
+			fi
+		else
+			if [ "$downsample" = true ]; then  
+				slack "training downsampled distilled DeepSTARR models with standard deviation prediction  exited with error code $exit_code"
+			else 
+				slack "training distilled DeepSTARR models with standard deviation prediction  exited with error code $exit_code"
+			fi
 		fi
 	fi
 fi
